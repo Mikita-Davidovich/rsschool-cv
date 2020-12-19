15503f84cb40ed41af5ee80880c52e0e15ddd122
Mikita-Davidovich <tosblrodo@gmail.com>
2020-12-19 15:37:15 +0300

    feat: add mouse click , cursor:pointer and user-select:none

--- DrumKit/main.js
+++ DrumKit/main.js
@@ -1,6 +1,6 @@
 //Play Song
 
-function playSound (e){
+function playSound (e) {
   const audio = document.querySelector(`audio[data-key = '${e.keyCode}']`);
   const key = document.querySelector(`.key[data-key = '${e.keyCode}']`);
   if(!audio) return;
@@ -11,14 +11,22 @@ function playSound (e){
 
 //Remove Transition
 
-function removeTransition(e){
+function removeTransition(e) {
   if(e.propertyName === 'transform') return;
   this.classList.remove('active');  
-  console.log(e)  //??
+}
+//Play song when i click 
+function mouseClick(e) {
+  const audio = document.querySelector(`audio[data-key="${e.currentTarget.dataset.key}"]`);
+  audio.currentTime = 0;
+  audio.play();
+  this.classList.add('active');
 }
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+keys.forEach(key => key.addEventListener('click', mouseClick));
+
 
 window.addEventListener('keydown' ,playSound);
 
