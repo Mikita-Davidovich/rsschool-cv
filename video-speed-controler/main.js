55a9629e876a614ba1734dff782f0686500b37ff
Mikita-Davidovich <tosblrodo@gmail.com>
2021-01-26 10:36:38 +0300

    feat: add const

--- video-speed-controler/main.js
+++ video-speed-controler/main.js
@@ -1,14 +1,14 @@
-const video = document.querySelector('.video'),
-      speed = document.querySelector('.speed'),
-      speedBar = document.querySelector('.speed-bar');
+const video = document.querySelector('.video');
+const speed = document.querySelector('.speed');
+const speedBar = document.querySelector('.speed-bar');
 
 function speedController(e) {
-  const y = e.pageY - speed.offsetTop,// pageY расстояние от верха документа до курсора - смещение сверху относительно родителя
-        percent = y / speed.offsetHeight,
-        min = 0.4,
-        max = 4,
-        height = Math.round(percent * 100) + '%',
-        playbackRate = percent * (max - min) + min;//получение скорости воспроизведения в пределах от 0,4 до 4
+  const y = e.pageY - speed.offsetTop;// pageY расстояние от верха документа до курсора - смещение сверху относительно родителя
+  const percent = y / speed.offsetHeight;
+  const min = 0.4;
+  const max = 4;
+  const height = Math.round(percent * 100) + '%';
+  const playbackRate = percent * (max - min) + min;//получение скорости воспроизведения в пределах от 0,4 до 4
   speedBar.style.height = height;
   speedBar.textContent = playbackRate.toFixed(2) + 'x';
   video.playbackRate = playbackRate;
