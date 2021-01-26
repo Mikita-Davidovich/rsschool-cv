10bfb37dcb453bbde81e05e1dc8e37f6c219c5eb
Mikita-Davidovich <tosblrodo@gmail.com>
2021-01-26 21:19:24 +0300

    feat: improve timer and game start

--- WhackAMole/main.js
+++ WhackAMole/main.js
@@ -8,17 +8,16 @@ const hardMode = document.querySelector('.hard-mode');
 const extremalMode = document.querySelector('.extremal-mode');
 const timer = document.querySelector('.time')
 let lastHole;
-let timeUp = false;// время истекло
+let timeUp = true;
 let score = 0;
 let sec = 10;
 const min = 0;
-timer.innerHTML = `${min}:${sec}`;
 
-function randomTime(min, max){
+function randomTime(min, max) {
   return  Math.round (Math.random() * (max - min) + min);
 }
 
-function randomHole(holes){
+function randomHole(holes) {
   const index = Math.floor(Math.random() * holes.length);
   const hole = holes[index];
   if(lastHole === hole){
@@ -28,61 +27,63 @@ function randomHole(holes){
   return hole;
 }
 
-function showMole(min, max){
+function showMole(min, max) {
   const time = randomTime(min, max);
   const hole = randomHole(holes);
   hole.classList.add('active');
-  setTimeout(() =>{
+  setTimeout(() => {
     hole.classList.remove('active');
     if(!timeUp) showMole(min, max);
   }, time)
 }
 
-function startGame(min, max){
+function startGame(min, max) {
+  if(!timeUp) return;
   scoreBoard.textContent = 0;
   timeUp = false;
+  changeTimer();
   showMole(min, max);
-  setTimeout(() => timeUp = true, 10000)  
+  setTimeout(() => timeUp = true, 10000);
 }
 
-function clickOnMole(e){
-  if(!e.isTrusted) return;// если действие было вызвано не пользователем
+function clickOnMole(e) {
+  if(!e.isTrusted) return;
   score++;
   this.parentNode.classList.remove('active');
   scoreBoard.textContent = score;
   localStorage.setItem('Score', scoreBoard.textContent);
 }
 
-function getScore (){
+function getScore () {
   scoreBoard.textContent = localStorage.getItem('Score') === null ? 0 : localStorage.getItem('Score');
 }
 
-function startStandartMode(){
+function startStandartMode() {
   startGame(300, 2000)
 }
 
-function startEaseMode(){
+function startEaseMode() {
   startGame(400, 1500);
 }
 
-function startMediumMode(){
+function startMediumMode() {
   startGame(250, 1000);
 }
 
-function startHardMode(){
+function startHardMode() {
   startGame(150, 800);
 }
 
-function startExtremalMode(){
+function startExtremalMode() {
   startGame(100, 500);
 }
 
-function startTimer(){
-  if(sec < 0) {
-    return
+function changeTimer() {
+  if (sec < 0) {
+    sec = 10;
   } else {
-  timer.innerHTML = `${min}:${addZero(sec--)}`;
-  setTimeout(startTimer, 1000); 
+    timer.innerHTML = `${min}:${addZero(sec--)}`;
+    setTimeout(changeTimer, 1000); 
   }
 }
 
@@ -92,14 +93,9 @@ function addZero(n) {
 
 moles.forEach(mole => mole.addEventListener('click',clickOnMole));
 standartMode.addEventListener('click' , startStandartMode);
-standartMode.addEventListener('click' , startTimer);
 easeMode.addEventListener('click', startEaseMode);
-easeMode.addEventListener('click', startTimer);
 mediumMode.addEventListener('click', startMediumMode);
-mediumMode.addEventListener('click', startTimer);
 hardMode.addEventListener('click', startHardMode);
-hardMode.addEventListener('click', startTimer);
 extremalMode.addEventListener('click', startExtremalMode);
-extremalMode.addEventListener('click', startTimer);
 getScore();
 addZero();
